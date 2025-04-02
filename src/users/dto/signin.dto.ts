1957ff36a5d2dfe345d126311c4bb498d3c5c432
Victor <victorclovisb@hotmail.com>
2025-04-02 10:15:32 -0300

    feat: Implement validation pipes and add SigninDto for user authentication

--- src/users/dto/signin.dto.ts
+++ src/users/dto/signin.dto.ts
@@ -1,10 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class SignupDto {
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-
+export class SigninDto {
   @IsString()
   @IsNotEmpty()
   @IsEmail()
