5da781677b64b5aaf34c814fb34dc3115fafafda
Victor <victorclovisb@hotmail.com>
2025-04-02 10:10:42 -0300

    feat: Add user schema with password hashing middleware

---
+++ src/users/schemas/users.schema.ts
@@ -0,0 +1,21 @@
+import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+export const UsersSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// middleware verifica se o password foi modificado e se sim, criptografa a senha
+UsersSchema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    this['password'] = await bcrypt.hash(this['password'], 10);
+  } catch (err) {
+    return next(err);
+  }
+});
