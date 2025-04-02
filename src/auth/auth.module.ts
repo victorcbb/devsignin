64a41510a5b766890f1b61d6db8837eca2bc6b55
Victor <victorclovisb@hotmail.com>
2025-04-02 10:23:42 -0300

    feat: Integrate Mongoose, Passport, and JWT modules into AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { UsersSchema } from 'src/users/schemas/users.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UsersSchema,
+      },
+    ]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+    }),
+  ],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
